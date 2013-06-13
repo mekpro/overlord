f9584525c79c214f3b3d939548a1b0f6d7fa0f42
Pongsakorn U-chupala <knightbaron@gmail.com>
2013-06-13 20:23:01 +0700

    Tidy server code & remove unused variables/imports

--- server/common.py
+++ server/common.py
@@ -1,15 +1,14 @@
-import datetime
+# import datetime
 from pymongo import MongoClient
-import pymongo
-import logging
-import random
+# import pymongo
+# import logging
+# import random
 import config
 
+
 def load_hostlist():
-  conn = MongoClient(config.MONGO_SERVER)[config.MONGO_DB]
-  hostlist = list()
-  for host in conn.host.find():
-    hostlist.append(host["hostname"])
-  return hostlist
-
-
+    conn = MongoClient(config.MONGO_SERVER)[config.MONGO_DB]
+    hostlist = list()
+    for host in conn.host.find():
+        hostlist.append(host["hostname"])
+    return hostlist
