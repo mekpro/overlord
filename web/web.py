76b1ff3027c5c02b4b5346d788c72bee516a5c2a
mekpro <mekpro@gmail.com>
2013-07-23 17:57:02 +0700

    refactored web code

--- web/web.py
+++ web/web.py
@@ -9,15 +9,43 @@ import logging
 import query
 import config
 
-@route('/assets/<filepath:path>')
+def api_dt_start(request):
+  if 'dt_start' not in request.GET:
+    dt_start = datetime.datetime.now() - datetime.timedelta(minutes=120)
+  else:
+    dt_start = query.dt_from_timestamp(float(request.GET['dt_start']))
+  return dt_start
+
+def api_dt_end(request):
+  if 'dt_end' not in request.GET:
+    dt_end = dt_start + datetime.timedelta(minutes=120)
+  else:
+    dt_end = query.dt_from_timestamp(float(request.GET['dt_end']))
+  return dt_end
+
+def web_dt_start(request):
+  if request.forms.get('dt_start') == '':
+    dt_start = datetime.datetime.now() - datetime.timedelta(hours=2)
+  else:
+    dt_start = query.parse_form_dt(request.forms.get('dt_start'))
+  return dt_start
+
+def web_dt_end(request):
+  if request.forms.get('dt_end') == '':
+    dt_end = dt_start + datetime.timedelta(hours=2)
+  else:
+    dt_end = query.parse_form_dt(request.forms.get('dt_end'))
+  return dt_end
+
+@get('/assets/<filepath:path>')
 def server_static(filepath):
   return static_file(filepath, root=config.STATIC_ROOT)
 
-@route('/')
+@get('/')
 def route_root():
   bottle.redirect("/index")
 
-@route('/index')
+@get('/index')
 def index_view():
   last_update = datetime.datetime.now()
   hostlist = query.hostlist()
@@ -25,15 +53,15 @@ def index_view():
   logging.error(graph)
   return template('index_template', title="Overlord Monitoring" ,graph=graph, hostlist=hostlist, last_update=last_update)
 
-@route('/chordgraph')
+@get('/chordgraph')
 def chordgraph_view():
   return template('chordgraph_template')
 
-@route('/calendar')
+@get('/calendar')
 def calendar_view():
   return template('calendar_template')
 
-@route('/heatmap/daily')
+@get('/heatmap/daily')
 def heatmap_daily_view():
   return template('heatmap_template')
 
@@ -49,16 +77,8 @@ def host_view(hostname):
 
 @post('/host/<hostname>')
 def host_view_post(hostname):
-  if request.forms.get('dt_start') == '':
-    dt_start = datetime.datetime.now() - datetime.timedelta(hours=2)
-  else:
-    dt_start = query.parse_form_dt(request.forms.get('dt_start'))
-
-  if request.forms.get('dt_end') == '':
-    dt_end = dt_start + datetime.timedelta(hours=2)
-  else:
-    dt_end = query.parse_form_dt(request.forms.get('dt_end'))
-
+  dt_start = web_dt_start(request)
+  dt_end = web_dt_end(request)
   last_update = datetime.datetime.now()
   hostlist = query.hostlist()
   ping_table = query.host_tables(hostname, metric='ping')
@@ -66,60 +86,42 @@ def host_view_post(hostname):
   return template('host_template', title=hostname, hostname=hostname, hostlist=hostlist, last_update=last_update, dt_start=query.dt_to_timestamp(dt_start), dt_end=query.dt_to_timestamp(dt_end), ping_table=ping_table, iperf_table=iperf_table)
 
 
-@route('/api')
+@get('/api')
 def api_index():
   return template('api_index')
 
-@route('/api/hostlist')
+@get('/api/hostlist')
 def api_hostlist():
   hostlist = query.hostlist()
   return {"result" : hostlist}
 
-@route('/api/hoststats')
+@get('/api/hoststats')
 def api_hoststats():
   hoststats = query.hoststats()
   return {"result" : hoststats}
 
-@route('/api/flowlist/<hostname>')
+@get('/api/flowlist/<hostname>')
 def api_flowlist(hostname):
   flowlist = query.flowlist(hostname)
   return {"result": flowlist}
 
-@route('/api/flowstats/<hostname>')
+@get('/api/flowstats/<hostname>')
 def api_flowstats(hostname):
   flowstats = query.flowstats(hostname)
   return {"result": flowstats}
 
-@route('/api/query/graph/<module>/<metric>')
+@get('/api/query/graph/<module>/<metric>')
 def api_query_graph(module,metric):
-  if 'dt_start' not in request.GET:
-    dt_start = datetime.datetime.now() - datetime.timedelta(minutes=120)
-  else:
-    dt_start = query.dt_from_timestamp(float(request.GET['dt_start']))
-
-  if 'dt_end' not in request.GET:
-    dt_end = dt_start + datetime.timedelta(minutes=120)
-  else:
-    dt_end = query.dt_from_timestamp(float(request.GET['dt_end']))
-
-  logging.error('query dt_start %s', str(dt_start))
-  logging.error('query dt_end %s', str(dt_end))
+  dt_start = api_dt_start(request)
+  dt_end = api_dt_end(request)
   count = 5
   graph = query.graph_query(module, metric, count, dt_start, dt_end)
   return {"result": graph}
 
-@route('/api/query/host/<hostname>/<module>/<metric>')
+@get('/api/query/host/<hostname>/<module>/<metric>')
 def api_query_host(hostname, module, metric):
-  if 'dt_start' not in request.GET:
-    dt_start = datetime.datetime.now() - datetime.timedelta(minutes=120)
-  else:
-    dt_start = query.dt_from_timestamp(float(request.GET['dt_start']))
-
-  if 'dt_end' not in request.GET:
-    dt_end = dt_start + datetime.timedelta(minutes=120)
-  else:
-    dt_end = query.dt_from_timestamp(float(request.GET['dt_end']))
-
+  dt_start = api_dt_start(request)
+  dt_end = api_dt_end(request)
   count = 5
   table = query.host_query(hostname, module, metric, count, dt_start, dt_end)
   return {"result": table}
